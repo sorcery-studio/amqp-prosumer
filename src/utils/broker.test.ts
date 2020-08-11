3f4c407257cb905e244cc45307831006614ad367
Grzeogrz Godlewski <grzegorz@sorcerystudio.com>
2020-08-11 21:48:09 +0200

    To-do tests for broker utils

---
+++ src/utils/broker.test.ts
@@ -0,0 +1,9 @@
+describe("Broker Utils", () => {
+  describe("Connecting to broker", () => {
+    test.todo("Connection can be established");
+  });
+
+  describe("Disconnecting from broker", () => {
+    test.todo("Connection can be closed");
+  });
+});
