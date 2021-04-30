72839bc6b650d37be57582974956a3d6c3f845d6
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2021-04-30 21:23:40 +0200

    Fixing issues with husky

--- src/index.test.ts
+++ src/index.test.ts
@@ -17,8 +17,8 @@ jest.mock("./app", () => {
 describe("Index", () => {
   test("it contains the shebang line", () => {
     // Try to get the "built JS file first
-    if (fs.existsSync("./index.js")) {
-      const script = fs.readFileSync("./index.js").toString();
+    if (fs.existsSync("./src/index.js")) {
+      const script = fs.readFileSync("./src/index.js").toString();
       expect(script).toContain("#!/usr/bin/env node");
     } else {
       const script = fs.readFileSync("./src/index.ts").toString();
